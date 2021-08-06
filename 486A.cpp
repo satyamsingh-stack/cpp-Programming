8c9cd15697be357aa735ab9fa945a8018fa91dca
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2021-08-06 19:32:24 +0530

    Create 486A.cpp

---
+++ 486A.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+ #include<cstdio>
+
+using namespace std;
+
+int main(){
+
+    long long n; 
+ cin >> n;
+
+    if(n % 2 == 0)cout << n/2 << endl; 
+ else cout << -(n+1)/2 << endl; 
+ return 0; 
+ }
